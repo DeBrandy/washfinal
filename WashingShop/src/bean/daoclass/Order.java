a717a28e06dc2b748c388adf8ade7373fa2d1de5
zhhxh <41145650+zhhxh@users.noreply.github.com>
2018-07-14 00:48:29 +0800

    12

--- WashingShop/src/bean/daoclass/Order.java
+++ WashingShop/src/bean/daoclass/Order.java
@@ -5,11 +5,8 @@ import java.util.Date;
 public class Order {
 	private String Oid,Cid,Time;//订单号，手机号,日期
 	private int Number;//数量
-<<<<<<< HEAD
-=======
-	private String Time;//日期
->>>>>>> 9a4e1f48faffa5b39defb0cf364fd5e413823fdd
 	private int Statue;
+	
 	public Order(){}
 	public String getOid() {
 		return Oid;
