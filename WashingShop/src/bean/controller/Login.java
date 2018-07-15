f0d7503f3c846b186a32d28c2e780040726aa633
qcga <41146166+qcga@users.noreply.github.com>
2018-07-15 20:22:49 +0800

    3333
    登录

--- WashingShop/src/bean/controller/Login.java
+++ WashingShop/src/bean/controller/Login.java
@@ -1,4 +1,5 @@
 package bean.controller;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
 import org.springframework.ui.Model;
@@ -9,12 +10,21 @@ import bean.service.AdminService;
 
 @Controller
 public class Login {
+	
+	@Autowired
+	private operator ope;
+
+	
 	@RequestMapping("/validLogin")
     public String login(@RequestParam("lname")String lname,@RequestParam("password")String password,Model model)
     {
-		
-		
-		
+		ope.setLname("快落洗衣");
+		ope.setPassword("Hss1999");
+		if(ope.getLname().equals(lname) && ope.getPassword().equals(password))
+		{	
+			model.addAttribute("ope", ope);  //加入到model数据类型中
+			return "home";
+		}
     	return "login";//验证失败，跳转回登录界面
     }
     
